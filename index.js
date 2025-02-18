d057d83ae849a2b8a9fdc3995ad737d7edf57420
billiraheem <Babdulraheem@cleanmeter.io>
2025-02-18 16:36:08 +0100

    Query variables and nested resolver functions

--- index.js
+++ index.js
@@ -8,18 +8,56 @@ import DB from './DB.js'
 import { typeDefs } from './schema.js'
 
 // has to match the query schema
+// Game is used to access related data to the game object like reviews for the game, the parent will be the game()
+// 'parent' refers to the parent resolver in a resolver chain
+// 'args' is used to access the query variable
+// 'context' is an object that can be used for spying context values across all our resolvers i.e auth
 const resolvers = {
     Query: {
         games() {
             return DB.games
         },
 
+        game(_, args) {
+            return DB.games.find((game) => game.id === args.id)
+        },
+
         authors() {
             return DB.authors
         },
 
+        author(_, args) {
+            return DB.authors.find((author) => author.id === args.id)
+        },
+
         reviews() {
             return DB.reviews
+        },
+
+        review(_, args) {
+            return DB.reviews.find((review) => review.id === args.id)
+        }
+    },
+
+    Game: {
+        reviews(parent) {
+            return DB.reviews.filter((review) => review.game_id === parent.id)
+        }
+    },
+
+    Author: {
+        reviews(parent) {
+            return DB.reviews.filter((review) => review.author_id === parent.id)
+        }
+    },
+
+    Review: {
+        author(parent) {
+            return DB.authors.find((a) => a.id === parent.author_id)
+        },
+
+        game(parent) {
+            return DB.games.find((g) => g.id === parent.game_id)
         }
     }
 }
--- schema.js
+++ schema.js
@@ -1,28 +1,36 @@
 // ! are used to show that a property is required and cannot be null
+// when there is ! after a list then it means the list cannot be null but if it is not after then the list can be null
 // Query type is something every graphql schema needs to have and what it does is to define the entry points to and specify the return types of those endpoints
 export const typeDefs = `#graphql
     type Game {
         id: ID!
         title: String!
         platform: [String!]!
+        reviews: [Review!]
     }
 
     type Review {
         id: ID!
         rating: Int!
         content: String!
+        game: Game!
+        author: Author!
     }
 
     type Author {
         id: ID!
         name: String!
         verified: Boolean!
+        reviews: [Review!]
     }
     
     type Query {
         games: [Game]
+        game(id: ID!): Game
         reviews: [Review]
+        review(id: ID!): Review
         authors: [Author]
+        author(id: ID!): Author
     }
 `
-
+// a query variable is used when we want to fetch a single data and the variable is used to find the data to be fetched
